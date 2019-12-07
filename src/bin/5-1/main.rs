21ce2f29c0e5d8ac49f636951509320381aae4b1
Chris West (Faux) <git@goeswhere.com>
2019-12-07 17:12:06 +0000

    5-1

---
+++ src/bin/5-1/main.rs
@@ -0,0 +1,64 @@
+use std::convert::TryFrom;
+
+fn main() {
+    let mut s = include_str!("input")
+        .split(',')
+        .map(|l| l.trim())
+        .filter(|l| !l.is_empty())
+        .map(|l| l.parse().unwrap())
+        .collect::<Vec<i64>>();
+
+    println!("{:?}", interpret(vec![1], &mut s));
+}
+
+fn param(s: &[i64], p: usize, n: usize) -> i64 {
+    let op = s[p];
+    assert!(op > 0);
+    let mode = op / 10i64.pow((n + 1) as u32) % 10;
+    match mode {
+        0 => s[addr(s[p + n])],
+        1 => s[p + n],
+        _ => unreachable!("bad opcode: {}", s[p]),
+    }
+}
+
+fn addr(val: i64) -> usize {
+    usize::try_from(val).expect("addr")
+}
+
+fn interpret(mut inputs: Vec<i64>, s: &mut [i64]) -> Vec<i64> {
+    let mut p = 0usize;
+    let mut outputs = Vec::new();
+    loop {
+        let ins = s[p];
+        match ins % 100 {
+            1 => {
+                let dest = addr(s[p + 3]);
+                s[dest] = param(s, p, 1) + param(s, p, 2);
+                p += 4;
+            }
+
+            2 => {
+                let dest = addr(s[p + 3]);
+                s[dest] = param(s, p, 1) * param(s, p, 2);
+                p += 4;
+            }
+
+            3 => {
+                let dest = addr(s[p + 3]);
+                s[dest] = inputs.remove(0);
+                p += 2;
+            }
+
+            4 => {
+                outputs.push(param(s, p, 1));
+                p += 2;
+            }
+
+            99 => break,
+
+            other => unreachable!("bad opcode: {}", other),
+        }
+    }
+    outputs
+}
