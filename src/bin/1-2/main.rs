482943a910b420acacaddb22565c1c0d4239f5a0
Chris West (Faux) <git@goeswhere.com>
2019-12-07 16:02:39 +0000

    1-2

--- src/bin/1-2/main.rs
+++ src/bin/1-2/main.rs
@@ -6,7 +6,16 @@ fn main() {
             .map(|l| l.trim())
             .filter(|l| !l.is_empty())
             .map(|l| l.trim().parse().unwrap())
-            .map(|d: u64| d / 3 - 2)
+            .map(fuel_for)
             .sum::<u64>()
     )
+}
+
+fn fuel_for(mass: u64) -> u64 {
+    if 0 == mass {
+        return 0;
+    }
+
+    let immediate = (mass / 3).saturating_sub(2);
+    immediate + fuel_for(immediate)
 }
